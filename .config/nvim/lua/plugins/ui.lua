2a51e167762f7f946c90564aa4378b4fdbcb82c7
Christoph Schmatzler <christoph.schmatzler@phrase.com>
2023-10-27 15:03:57 +0200

    copilot pog

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,12 +1,4 @@
 return {
   -- Disable built-in plugins
   { "akinsho/bufferline.nvim", enabled = false },
-
-  -- mini-starter
-  {
-    "echasnovski/mini.starter",
-    opts = function(_, opts)
-      opts.header = ""
-    end,
-  },
 }
