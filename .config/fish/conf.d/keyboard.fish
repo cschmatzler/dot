7623fbacee859fa0fff194913ca1c162829fadee
Christoph Schmatzler <christoph.schmatzler@phrase.com>
2023-12-05 09:53:07 +0100

    stuff

--- .config/fish/conf.d/keyboard.fish
+++ .config/fish/conf.d/keyboard.fish
@@ -1,4 +1,4 @@
 for mode in default insert
   bind --mode $mode \ch _atuin_search
-  bind --mode $mode \cd cdi
+  bind --mode $mode \cd br
 end
--- .config/fish/functions/br.fish
+++ .config/fish/functions/br.fish
@@ -1,1 +1,12 @@
-/Users/christophschmatzler/Library/Application Support/org.dystroy.broot/launcher/fish/br.fish
+function br --wraps=broot
+      set -l cmd_file (mktemp)
+      if broot --outcmd $cmd_file $argv
+          read --local --null cmd < $cmd_file
+          rm -f $cmd_file
+          eval $cmd
+      else
+          set -l code $status
+          rm -f $cmd_file
+          return $code
+      end
+  end
