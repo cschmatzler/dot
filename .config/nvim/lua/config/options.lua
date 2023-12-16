4d06be531caa1d19256b650fd4f0d506cf777b41
Christoph Schmatzler <christoph.schmatzler@phrase.com>
2023-12-16 13:04:42 +0100

    tabs > spaces

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,5 +1,9 @@
+local opt = vim.opt
+
 vim.g.autoformat = false
 vim.g.root_spec = { ".git", "lsp", "cwd" }
-vim.opt.formatoptions = "jcroqlnt"
-vim.opt.textwidth = 110
-vim.opt.colorcolumn = "111"
+opt.formatoptions = "jcroqlnt"
+opt.textwidth = 110
+opt.colorcolumn = "111"
+opt.expandtab = false
+opt.shiftwidth = 4
