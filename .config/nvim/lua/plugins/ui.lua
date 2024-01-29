e4327669264b58e2846f218a3b2c8a190b380d8f
Christoph Schmatzler <christoph.schmatzler@phrase.com>
2024-01-29 15:52:05 +0100

    huh

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,4 +1,47 @@
 return {
   -- Disable built-in plugins
   { "akinsho/bufferline.nvim", enabled = false },
+  {
+    "folke/edgy.nvim",
+    event = "VeryLazy",
+    opts = {
+      animate = {
+        enabled = false,
+      },
+      left = {
+        {
+          title = "Tree",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "filesystem"
+          end,
+          pinned = true,
+          open = function()
+            vim.api.nvim_input("<esc><space>e")
+          end,
+          size = { height = 0.5 },
+        },
+        {
+          title = "Buffers",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "buffers"
+          end,
+          pinned = true,
+          open = "Neotree position=top buffers",
+        },
+      },
+      bottom = {
+        "Trouble",
+        {
+          ft = "trouble",
+          filter = function(buf, win)
+            return vim.api.nvim_win_get_config(win).relative == ""
+          end,
+        },
+        { title = "Search and replace", ft = "spectre_panel", size = { height = 0.4 } },
+        { title = "Test output", ft = "neotest-output-panel", size = { height = 15 } },
+      },
+    },
+  },
 }
