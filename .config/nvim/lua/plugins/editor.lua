b8fe42f58c4f676e5a03d06e38baf6b8fbbe2e98
Christoph Schmatzler <christoph@medium.place>
2024-03-06 19:15:43 +0100

    oops

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -38,24 +38,6 @@ return {
 		},
 	},
 	{
-		"NeogitOrg/neogit",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"sindrets/diffview.nvim",
-			"nvim-telescope/telescope.nvim",
-		},
-		keys = {
-			{
-				"<leader>gg",
-				function()
-					require("neogit").open()
-				end,
-				desc = "Neogit",
-			},
-		},
-		config = true,
-	},
-	{
 		"mbbill/undotree",
 		keys = {
 			{
