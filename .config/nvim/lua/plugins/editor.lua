e2f9d6fd65fb491c57d6f96cd59acec410de9249
Christoph Schmatzler <christoph.schmatzler@phrase.com>
2024-03-13 20:06:49 +0100

    oil settings

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -7,6 +7,8 @@ return {
       { "<leader>e", "<cmd>Oil<cr>", desc = "Files" },
     },
     opts = {
+      skip_confirm_for_simple_edits = true,
+      experimental_watch_for_changes = true,
       view_options = {
         show_hidden = true,
       },
