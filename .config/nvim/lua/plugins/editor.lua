3b25181c4e4ea08edd9a9870fa5b455dd4c53f39
Christoph Schmatzler <christoph@medium.place>
2024-03-17 20:00:18 +0100

    back to lg

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -40,26 +40,4 @@ return {
       },
     },
   },
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-    keys = {
-      {
-        "<leader>gg",
-        function()
-          require("neogit").open()
-        end,
-        desc = "neogit",
-      },
-    },
-    opts = {
-      integrations = {
-        diffview = true,
-      },
-    },
-  },
 }
