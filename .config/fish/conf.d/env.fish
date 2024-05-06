e64d12491facbdea600f393edfd846c015a856a5
Christoph Schmatzler <christoph@medium.place>
2024-05-06 09:53:17 +0200

    fzf

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -5,3 +5,9 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 
 # rg
 set -gx RIPGREP_CONFIG_PATH ~/.config/ripgrep/config
+
+# fzf
+set -Ux FZF_DEFAULT_OPTS "\
+  --color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
+  --color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
+  --color=marker:#dc8a78,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39"
--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -2,6 +2,7 @@ return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
   { "echasnovski/mini.comment", enabled = false },
+  { "nvim-telescope/telescope.nvim", enabled = false },
   {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -17,20 +18,26 @@ return {
     },
   },
   {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-
-        return {
-          file_ignore_patterns = {
-            "target/**/*",
-          },
-        }
-      end,
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { "<leader>ff", "<cmd>FzfLua git_files<cr>", desc = "Find GitFiles (Root Dir)" },
+      { "<leader>fF", "<cmd>FzfLua files<cr>", desc = "Find Files (Root Dir)" },
+      { "<leader>fr", "<cmd>FzfLua oldfiles<cr>", desc = "Recent" },
+      { "<leader>sg", "<cmd>FzfLua live_grep<cr>", desc = "Grep (Root Dir)" },
+      { "<leader>ss", "<cmd>FzfLua lsp_document_symbols<cr>", desc = "Goto Symbol" },
+      { "<leader>sS", "<cmd>FzfLua lsp_workspace_symbols<cr>", desc = "Goto Symbol (Workspace)" },
     },
+    config = function()
+      require("fzf-lua").setup({
+        "telescope",
+        oldfiles = {
+          cwd_only = true,
+        },
+      })
+
+      require("fzf-lua").register_ui_select()
+    end,
   },
   {
     "mbbill/undotree",
