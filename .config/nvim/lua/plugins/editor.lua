b3d74c33d9d0e8bad3e3bee1ac95cf674bd78fcd
Christoph Schmatzler <christoph@medium.place>
2024-05-06 12:19:11 +0200

    -fzf +telescope

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,8 +1,7 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
-  { "echasnovski/mini.comment", enabled = false },
-  { "nvim-telescope/telescope.nvim", enabled = false },
+  { "echasnovski/mini.pairs",      enabled = false },
+  { "echasnovski/mini.comment",    enabled = false },
   {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -16,28 +15,6 @@ return {
         show_hidden = true,
       },
     },
-  },
-  {
-    "ibhagwan/fzf-lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    keys = {
-      { "<leader>ff", "<cmd>FzfLua git_files<cr>", desc = "Find GitFiles (Root Dir)" },
-      { "<leader>fF", "<cmd>FzfLua files<cr>", desc = "Find Files (Root Dir)" },
-      { "<leader>fr", "<cmd>FzfLua oldfiles<cr>", desc = "Recent" },
-      { "<leader>sg", "<cmd>FzfLua live_grep<cr>", desc = "Grep (Root Dir)" },
-      { "<leader>ss", "<cmd>FzfLua lsp_document_symbols<cr>", desc = "Goto Symbol" },
-      { "<leader>sS", "<cmd>FzfLua lsp_workspace_symbols<cr>", desc = "Goto Symbol (Workspace)" },
-    },
-    config = function()
-      require("fzf-lua").setup({
-        "telescope",
-        oldfiles = {
-          cwd_only = true,
-        },
-      })
-
-      require("fzf-lua").register_ui_select()
-    end,
   },
   {
     "mbbill/undotree",
