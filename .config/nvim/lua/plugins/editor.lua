7cfaa25b67c000f2f525085fcee12237b81517e6
Christoph Schmatzler <christoph@medium.place>
2024-06-04 14:30:59 +0200

    up

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -31,4 +31,20 @@ return {
     event = "VeryLazy",
     opts = {},
   },
+  {
+    "echasnovski/mini.move",
+    event = "BufReadPost",
+    opts = {
+      mappings = {
+        left = "<M-Left>",
+        right = "<M-Right>",
+        down = "<M-Down>",
+        up = "<M-Up>",
+        line_left = "<M-Left>",
+        line_right = "<M-Right>",
+        line_down = "<M-Down>",
+        line_up = "<M-Up>",
+      },
+    },
+  },
 }
