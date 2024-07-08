405e4f364fe107d66d0d7346e040fbed252f25eb
Christoph Schmatzler <christoph@medium.place>
2024-07-08 22:18:13 +0200

    supermaven?

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,12 +1,26 @@
 return {
   {
     "hrsh7th/nvim-cmp",
+    enabled = false,
+    dependencies = {
+      "onsails/lspkind-nvim",
+    },
     opts = function(_, opts)
       local cmp = require("cmp")
 
       opts.mapping = cmp.mapping.preset.insert({
         ["<C-y>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
       })
+
+      opts.formatting = {
+        format = require("lspkind").cmp_format({
+          mode = "symbol",
+          max_width = 50,
+          symbol_map = { Supermaven = "" },
+        }),
+      }
+
+      table.insert(opts.sources, { name = "supermaven" })
     end,
   },
   {
@@ -18,5 +32,14 @@ return {
     config = function()
       vim.api.nvim_set_hl(0, "LazyGitBorder", { fg = "#1e66f5" })
     end,
+  },
+  {
+    "supermaven-inc/supermaven-nvim",
+    opts = {
+      keymaps = {
+        accept_suggestion = "<C-y>",
+        accept_word = "<Tab>",
+      },
+    },
   },
 }
