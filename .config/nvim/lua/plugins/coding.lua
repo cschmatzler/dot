2eda2af9016896e2e12748249aff25b549900c11
Christoph Schmatzler <christoph@medium.place>
2024-07-29 15:21:05 +0200

    - supermaven

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,7 +1,6 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    enabled = false,
     dependencies = {
       "onsails/lspkind-nvim",
     },
@@ -11,16 +10,6 @@ return {
       opts.mapping = cmp.mapping.preset.insert({
         ["<C-y>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
       })
-
-      opts.formatting = {
-        format = require("lspkind").cmp_format({
-          mode = "symbol",
-          max_width = 50,
-          symbol_map = { Supermaven = "" },
-        }),
-      }
-
-      table.insert(opts.sources, { name = "supermaven" })
     end,
   },
   {
@@ -32,14 +21,5 @@ return {
     config = function()
       vim.api.nvim_set_hl(0, "LazyGitBorder", { fg = "#1e66f5" })
     end,
-  },
-  {
-    "supermaven-inc/supermaven-nvim",
-    opts = {
-      keymaps = {
-        accept_suggestion = "<C-y>",
-        accept_word = "<Tab>",
-      },
-    },
   },
 }
