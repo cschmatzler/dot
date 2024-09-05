70ded7d94c2ea5575e23f69ec7b00d0c6fc67258
Christoph Schmatzler <christoph@medium.place>
2024-09-05 11:03:07 +0200

    fix fisher and theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,3 @@
-set -g fisher_path $HOME/.local/share/fish/fisher
 set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
 set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
 for file in $fisher_path/conf.d/*.fish
@@ -7,9 +6,6 @@ end
 if status is-interactive && ! functions --query fisher
     curl --silent --location https://git.io/fisher | source && fisher install jorgebucaran/fisher
 end
-
-set -gx ZELLIJ_AUTO_ATTACH true
-set -gx ZELLIJ_AUTO_EXIT true
 
 if status is-interactive
     set -U FZF_COMPLETE 0
