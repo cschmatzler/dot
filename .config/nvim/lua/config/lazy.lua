4e6845bb8f58a57719144025d1f806c5f79f0ed6
Christoph Schmatzler <christoph@medium.place>
2024-11-01 19:46:29 +0100

    up

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.ruby" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
--- .config/nvim/lua/plugins/lang.lua
+++ .config/nvim/lua/plugins/lang.lua
@@ -1,6 +1,29 @@
 return {
-  -- Ruby
-  {},
+  -- Swift
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "swift",
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        sourcekit = {
+          capabilities = {
+            workspace = {
+              didChangeWatchedFiles = {
+                dynamicRegistration = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
   -- Elixir
   {
     "nvim-treesitter/nvim-treesitter",
