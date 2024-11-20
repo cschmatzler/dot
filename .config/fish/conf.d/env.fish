e00255ceb181b6ec0201f2bf5d1ad9e9280e5593
Christoph Schmatzler <christoph@medium.place>
2024-11-20 09:43:43 +0100

    up

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -2,7 +2,7 @@ set -gx EDITOR nvim
 set -gx VISUAL $EDITOR
 set -gx PAGER ov
 set -gx XDG_CONFIG_HOME $HOME/.config
-set -gx ZK_NOTEBOOK_DIR ~/Notes
+set -gx ZK_NOTEBOOK_DIR ~/Notebook
 
 # rg
 set -gx RIPGREP_CONFIG_PATH ~/.config/ripgrep/config
